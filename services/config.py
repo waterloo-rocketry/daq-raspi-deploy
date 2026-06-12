52b3f60d7b42c5217e38b1dafafb4ee9bdd7efcf
Chris Yang <chrisyx511@gmail.com>
2026-06-11 22:07:43 -0400

    active config for S26 CF 1

--- services/config.py
+++ services/config.py
@@ -19,9 +19,23 @@ def setup():
     Setup the sensors.
     """
     # IFM (round)
-    Sensor("[OPT 102] Ox Fill (psi)", "AIN87", 10, Connection.SINGLE,
+    Sensor("[OPT-101] Ox Fill", "AIN87", 10, Connection.SINGLE,
+          LinearCalibration(1/100*1450/0.016, -0.004*1450/0.016, "psi"))
+    Sensor("[OPT 102] Ox Fill", "AIN95", 10, Connection.SINGLE,
+          LinearCalibration(1/100*1450/0.016, -0.004*1450/0.016, "psi"))
+    Sensor("[NPT-201] N2 Fill", "AIN86", 10, Connection.SINGLE,
+          LinearCalibration(1/100*1450/0.016, -0.004*1450/0.016, "psi"))
+    Sensor("[NPT-202] Pneumatics", "AIN94", 10, Connection.SINGLE,
+          LinearCalibration(1/100*1450/0.016, -0.004*1450/0.016, "psi"))
+    Sensor("[OPT-301] Ox Tank", "AIN85", 10, Connection.SINGLE,
+          LinearCalibration(1/100*1450/0.016, -0.004*1450/0.016, "psi"))
+    Sensor("[FPT-301] Fuel Tank", "AIN93", 10, Connection.SINGLE,
+          LinearCalibration(1/100*1450/0.016, -0.004*1450/0.016, "psi"))
+    Sensor("[OPT-302] Ox Manifold", "AIN84", 10, Connection.SINGLE,
+          LinearCalibration(1/100*1450/0.016, -0.004*1450/0.016, "psi"))
+    Sensor("[FPT-302] Fuel Manifold", "AIN92", 10, Connection.SINGLE,
           LinearCalibration(1/100*1450/0.016, -0.004*1450/0.016, "psi"))
     # TDM (square)
-    Sensor("[OPT 101] Ox Fill (psi)", "AIN86", 10, Connection.SINGLE,
-          LinearCalibration(1/100*3000/0.016, -0.004*3000/0.016, "psi"))
+    #Sensor("[OPT 101] Ox Fill (psi)", "AIN86", 10, Connection.SINGLE,
+    #      LinearCalibration(1/100*3000/0.016, -0.004*3000/0.016, "psi"))
 
